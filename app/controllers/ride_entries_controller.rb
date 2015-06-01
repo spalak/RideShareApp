9c4bd5ff5c09e7f3ae2495a96d167233002c5bfc
Syam Palakurthy <syampalakurthy@gmail.com>
2015-05-31 17:24:38 -0700

    Updated ride controller and new view to remove the reference to a second model.  I'll add that back in eventually but first want to make sure my form successfully creates new ride entry objects.

--- app/controllers/ride_entries_controller.rb
+++ app/controllers/ride_entries_controller.rb
@@ -6,4 +6,20 @@ class RideEntriesController < ApplicationController
 	def new
 		@ride_entry = RideEntry.new
 	end
+
+	def create
+		@ride_entry = RideEntry.new(ride_entry_params)
+		if @ride_entry.save
+			flash[:success] = "Added new ride entry"
+			redirect_to ride_entries_path
+		else
+			flash[:error] = "There was a problem saving the ride entry"
+			render action: :new
+		end
+	end
+
+	private
+	def ride_entry_params
+		params[:ride_entry].permit(:content)
+	end
 end
